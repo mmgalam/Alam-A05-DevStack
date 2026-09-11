1672f83193c67b7db27a44f11471b81ea2ae40d3
mmgalam <mirmdgawharalam@gmail.com>
2026-09-12 01:33:50 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-5xl text-red-500'>Bismillah</h2>
+      <button className="btn">Default</button>
+    </>
+  )
+}
+
+export default App
