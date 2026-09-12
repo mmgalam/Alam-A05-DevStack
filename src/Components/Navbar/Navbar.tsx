15489c4492c4d7a40ad56cb2577df1e567662434
mmgalam <mirmdgawharalam@gmail.com>
2026-09-12 21:20:56 +0600

    Navbar Responsive Update

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,12 +1,18 @@
+import { FaBars } from 'react-icons/fa'
 import Logo from '../../assets/logo-text.png'
 
 export default function Navbar() {
     
     return (
         <nav className='flex justify-between container mx-auto items-center my-2 py-3'>
-            <img src={Logo} alt="Logo" />
 
-            <ul className='flex gap-4'>
+            <button className='md:hidden text-xl'>
+                <FaBars />
+            </button>
+            
+            <img src={Logo} alt="Logo" className='w-32' />
+
+            <ul className='hidden md:flex gap-4'>
                 <li className='text-orange-500'><a href="#">Home</a></li>
                 <li><a href="#">Technologies</a></li>
                 <li><a href="#">Projects</a></li>
@@ -18,6 +24,8 @@ export default function Navbar() {
                 <button>Sign In</button>
                 <button className='bg-[#D91B7E] text-white px-3 py-1.5 rounded-2xl'>Sign Up</button>
             </div>
+
+            
         </nav>
     )
 }
